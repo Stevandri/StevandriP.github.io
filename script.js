f4391c071b0c7702a12c311d7953566c5eb5d6a4
Stevandri <stevandripatty026@student.unsrat.ac.id>
2023-09-19 12:19:11 +0800

    first commit

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ if (!location.hash) {
 const roomHash = location.hash.substring(1);
 
 // TODO: Replace with your own channel ID
-const drone = new ScaleDrone('yiS12Ts5RdNhebyM');
+const drone = new ScaleDrone('aAtWcSz9alZrx1py');
 // Room name needs to be prefixed with 'observable-'
 const roomName = 'observable-' + roomHash;
 const configuration = {
